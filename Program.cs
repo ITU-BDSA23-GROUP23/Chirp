8c09aa9f14bc82304d5be412ef9d41ce3bc4a81b
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-08-31 16:17:36 +0200

    Comments about the code

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ if (args[0] == "read")
     var lines = File.ReadLines("chirp_cli_db.csv");
     foreach (var line in lines)
     {
+        //Should format and prints all cheeps, but splits incorrectly i.e. in the cheep itself
         string[] toRead = line.Split(",");
         string author = toRead[0];
         string message = toRead[1];
@@ -17,11 +18,14 @@ if (args[0] == "read")
 else if (args[0] == "cheep")
 {
     string cheep = "";
+    //Enables cheeps with spaces
     for (int i = 1; i < args.Length; i++)
     {
         cheep += " " + args[i];
     }
+    //Takes username from computer
     string author = Environment.UserName;
-    string timestamp = ("" + DateTime.Now);
+    
+    string timestamp = ("" + DateTime.Now); //To-do: Format like csv file example
     Console.WriteLine(cheep + " " + author + " " + timestamp);
 }
