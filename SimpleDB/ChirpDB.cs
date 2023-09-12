96af4653b560a6574be35aaa4b77dfcb282b9a4e
DHLarsen <daniel.holm.larsen@gmail.com>
2023-09-12 14:35:55 +0200

    Framework of DB
    Co-authored-by Nicklas <NGAR@itu.dk>

---
+++ SimpleDB/ChirpDB.cs
@@ -0,0 +1,47 @@
+﻿namespace SimpleDB;
+public class ChirpDB : IDatabaseRepository<Cheep>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        using var reader = new StreamReader("chirp_cli_db.csv");
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        var records = csv.GetRecords<Cheep>();
+        foreach (var record in records) 
+        {
+            Console.WriteLine($"{record.Id} @ {record.Name}: {record.Time}");
+        } 
+    }
+    public void Store(T record)
+    {
+        string author = Environment.UserName; //Takes username from computer
+        long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        string cheepString;
+        
+        ArrayList cheepList = new ArrayList();
+        
+       
+        foreach (String word in message)
+        {
+            cheepList.Add(word);
+        }
+        cheepString = string.Join(" ", cheepList.ToArray());
+        Console.WriteLine(author + ",\"" + cheepString + "\"," + timestamp);
+
+        var record = new List<Cheep>
+        {
+            new Cheep {Id = author, Name = cheepString, Time = timestamp}
+        };
+
+         var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        // Don't write the header again.
+        HasHeaderRecord = false,
+    };
+        using var stream = File.Open("chirp_cli_db.csv", FileMode.Append);
+        using var writer = new StreamWriter(stream);
+        using (var csv = new CsvWriter(writer, config))
+        {
+            csv.WriteRecords(record);
+        };
+    }
+}
