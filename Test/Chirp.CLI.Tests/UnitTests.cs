c1a67fe15d40a07663e2e41b75934b46392ccb7b
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-09-15 14:22:10 +0200

    Storing data correctly Unit Test

--- Test/Chirp.CLI.Tests/UnitTests.cs
+++ Test/Chirp.CLI.Tests/UnitTests.cs
@@ -3,29 +3,44 @@ namespace Chirp.CLI.Tests;
 
 public class UnitTests
 {
-    /*
     Program program;
     UserInterface userInterface;
+
     [Fact]
     public void IsDataStoredCorrectTest()
     {
         // Arrange
-        IEnumerable<string> message = new List<string> { "hej" };
+        IEnumerable<string> message = new List<string> { "test" };
         var program = new Program();
 
-        // Create a mock database for testing purposes
         var dbPath = "test_db.csv";
-        var mockDb = new ChirpDB(dbPath);
+        var mockDb = new ChirpDB(dbPath); // Creating a mock database to test
 
         // Act
         program.SaveCheep(message, mockDb);
 
         // Assert
-        var savedCheep = "";
-        Assert.Equal("hej", savedCheep);
+        var savedCheep = mockDb.Read();
+        Assert.Single(savedCheep); //This is while we assume we only have 1 cheep for this unit test
+        Assert.Equal(message, savedCheep[0].Name); //we say .Name because we want to compare first Cheep with msg
 
-        // Clean up the test database file
-        File.Delete(dbPath);
+        File.Delete(dbPath);  // Clean up the test database file
     }
-    */
+
+    [Fact]
+    public void TestTimestampConversion()
+    {
+        // Arrange
+        long unixTimestamp = 1631712052 //This is for examples
+        string expectedFormattedTime = "2021-09-16 09:20:52";
+        userInterface = new UserInterface();
+        /*long unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds(); //Our method for unixTimestamp in SaveCheep() in Program
+        string expectedFormattedTime = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd HH:mm:ss"); */
+
+        // Act
+        var formattedTime = userInterface.ConvertFromUnixTime(unixTimestamp);
+
+        // Assert
+        Assert.Equal(expectedFormattedTime, formattedTime);
+    }
 }
