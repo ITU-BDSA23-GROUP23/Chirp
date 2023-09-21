0dd966aa5e9a7551244d096ca5a73d0cd1e45407
tpep <tpep@itu.dk>
2023-09-21 14:49:59 +0200

    Sequencialized tests regarding database tampering

--- Test/Chirp.CLI.Tests/IntegrationTests.cs
+++ Test/Chirp.CLI.Tests/IntegrationTests.cs
@@ -4,8 +4,8 @@ using System.IO;
 
 namespace Chirp.CLI.Tests;
 
-/* // SEE BOTTOM OF ENDTOENDTESTS.cs!!!!!!!!!!!!
-[CollectionDefinition("Filetampering")]
+// SEE BOTTOM OF ENDTOENDTESTS.cs!!!!!!!!!!!!
+[Collection("Filetampering")]
 
 public class IntegrationTests
 {
@@ -13,40 +13,65 @@ public class IntegrationTests
     [Fact]
     public void WriteReadIntegrationTest()
     {
-        //Arrange
-        String originalWorkingDirectory = Environment.CurrentDirectory;
-        string simulatedDirectory = Path.Combine(originalWorkingDirectory, "../../../../../src/SimpleDB"); ;
-        Directory.SetCurrentDirectory(simulatedDirectory);
 
+        // Arrange
+        string databaseFilePath = "../../../../../src/SimpleDB/chirp_cli_db.csv";
+        string databaseCopyFilePath = "databaseTestCopy.csv";
 
-
-        StringWriter sw = new StringWriter();
-        Console.SetOut(sw);
-        string message = "hejtest";
-
-        //Act
 
         try
         {
-            Program.SaveCheep(message);
-            Program.Read();
+            // Copy the file from the source to the destination
+            File.Copy(databaseFilePath, databaseCopyFilePath, true);
+
+            Console.WriteLine("File copied successfully.");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"An error occurred while copying the file: {e.Message}");
+        }
+
+        string originalWorkingDirectory = Environment.CurrentDirectory;
+
+        try
+        {
+            string simulatedDirectory = Path.Combine(originalWorkingDirectory, "../../../../../src/SimpleDB"); ;
+            Directory.SetCurrentDirectory(simulatedDirectory);
+
+
+
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+            string message = "hejtest";
+
+            //Act
+
+            try
+            {
+                Program.SaveCheep(message);
+                Program.Read();
+            }
+            catch (Exception e)
+            {
+                Assert.Equal("Act failed!", e.Message);
+            }
+
+
+
+            //Assert
+            string expectedOutput = "hejtest";
+            //"Ord 1 Ord 2 Ord 3";
+            string[] outputLines = sw.ToString().Trim().Split("\n");
+            string lastLine = outputLines[0];
+            Assert.Contains(expectedOutput, lastLine);
         }
         catch (Exception e)
+        { Assert.Equal("Act failed!", e.Message); }
+        finally
         {
-            Assert.Equal("Act failed!", e.Message);
+            Directory.SetCurrentDirectory(originalWorkingDirectory);
+            File.Copy(databaseCopyFilePath, databaseFilePath, true);
+            File.Delete(databaseCopyFilePath);
         }
-
-
-
-        //Assert
-        string expectedOutput = "hejtest";
-        //"Ord 1 Ord 2 Ord 3";
-        string[] outputLines = sw.ToString().Trim().Split("\n");
-        string lastLine = outputLines[0];
-        Assert.Contains(expectedOutput, lastLine);
-        sw.Close();
-        Directory.SetCurrentDirectory(originalWorkingDirectory);
-
     }
 }
-*/
