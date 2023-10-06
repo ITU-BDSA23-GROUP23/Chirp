eb1fc01196198e39134bda672bcc475d6426b97c
tpep <tpep@itu.dk>
2023-10-06 13:43:23 +0200

    Made max32 Query
    Co-authored-by: ngar <ngar@itu.dk>

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,11 +1,13 @@
 using Chirp.Razor;
 using Chirp.Razor.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Identity.Client;
+using NuGet.Packaging.Signing;
 
 
 public interface ICheepService
 {
-    public List<Cheep> GetCheeps(int? page);
+    public List<Cheep> GetCheeps(int page);
     // public List<DBFacade.CheepViewModel> GetCheepsFromAuthor(string author);
     public List<Cheep> GetCheepsFromAuthor(int test);
 }
@@ -13,12 +15,14 @@ public interface ICheepService
 public class CheepService : ICheepService
 {
     private readonly ChirpDBContext context;
-    public CheepService(ChirpDBContext context) {
+    public CheepService(ChirpDBContext context)
+    {
         this.context = context;
         //context = new();
     }
-    public List<Cheep> GetCheeps(int? page)
+    public List<Cheep> GetCheeps(int page)
     {
+        var skip = 0;
         // context.Add(new Cheep{
         //     Author = new Author{
         //     Name = "TesterGuy",
@@ -28,20 +32,25 @@ public class CheepService : ICheepService
         // });
         // context.SaveChanges();
 
-        return context.Cheeps.ToList();
+        skip = (page - 1) * 32;
+
+        var cheeps = context.Cheeps.OrderByDescending(t => t.TimeStamp).Skip(skip).Take(32);
+
+        return cheeps.ToList();
     }
 
 
 
-    public List<Cheep> GetCheepsFromAuthor(int test) {
+    public List<Cheep> GetCheepsFromAuthor(int test)
+    {
 
-       
+
 
         return context.Cheeps.Where(r => r.Id == test).ToList();
-        
-        
+
+
     }
-    
+
 
     // public List<DBFacade.CheepViewModel> GetCheepsFromAuthor(string author)
     // {
