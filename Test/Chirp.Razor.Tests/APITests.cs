e497cee469a889aa745639fa0226c7db274ecd36
ngar <ngar@itu.dk>
2023-10-10 14:16:54 +0200

    Test for checking page 1 and 2 are not the same
    Co-Authored-By: Daniel Holm Larsen <103388012+DHLarsen@users.noreply.github.com>
    Co-Authored-By: Edward Rostomian <36159443+EdwardR7@users.noreply.github.com>

--- Test/Chirp.Razor.Tests/APITests.cs
+++ Test/Chirp.Razor.Tests/APITests.cs
@@ -5,11 +5,11 @@ public class APITests
 {
 
     //Credit and see more: https://zetcode.com/csharp/getpostrequest/?utm_content=cmp-true
-    [Fact(Skip = "Temporary skip")]
+    [Fact]
     public void HtmlVerificationForEndpointsPublic()
     {
         // Arrange
-        var url = "http://localhost:5273/"; //public timeline
+        var url = "http://localhost:5273/?page=21"; //public timeline
         var request = WebRequest.Create(url);
         request.Method = "GET";
 
@@ -19,17 +19,18 @@ public class APITests
 
         using var reader = new StreamReader(webStream);
         var HttpResponseBody = reader.ReadToEnd(); //We read all the data from the stream.
+        
 
         // Assert
         Assert.Contains("Hello, BDSA students!", HttpResponseBody);
         //Console.WriteLine(HttpResponseBody);
     }
 
-    [Fact(Skip = "Temporary skip")]
+    [Fact]
     public void HtmlVerificationForEndpointsPrivate()
     {
         // Arrange
-        var url = "http://localhost:5273/12"; //private timeline, may need to be /Rasmus later.
+        var url = "http://localhost:5273/Rasmus"; //private timeline, may need to be /Rasmus later.
         var request = WebRequest.Create(url);
         request.Method = "GET";
 
@@ -43,6 +44,37 @@ public class APITests
         // Assert
         Assert.Contains("Hej, velkommen til kurset.", HttpResponseBody);
         //Console.WriteLine(HttpResponseBody);
+    }
+
+    [Fact]
+    public void diffrentPageNotSameTest()
+    {
+         // Arrange
+        var url = "http://localhost:5273/?page=1"; //public timeline
+        var request = WebRequest.Create(url);
+        request.Method = "GET";
+
+        var url2 = "http://localhost:5273/?page=2"; //public timeline
+        var request2 = WebRequest.Create(url2);
+        request2.Method = "GET";
+
+
+        // Act
+        using var webResponse = request.GetResponse(); //returns a web response containing the response to the request.
+        using var webStream = webResponse.GetResponseStream(); //gets the instance of the stream class for reading data
+
+        using var reader = new StreamReader(webStream);
+        var HttpResponseBody = reader.ReadToEnd(); //We read all the data from the stream.
+
+        using var webResponse2 = request2.GetResponse(); //returns a web response containing the response to the request.
+        using var webStream2 = webResponse2.GetResponseStream(); //gets the instance of the stream class for reading data
+
+        using var reader2 = new StreamReader(webStream2);
+        var HttpResponseBody2 = reader2.ReadToEnd(); //We read all the data from the stream.
+
+        Assert.NotEqual(HttpResponseBody2, HttpResponseBody);
+
+
     }
 
 }
