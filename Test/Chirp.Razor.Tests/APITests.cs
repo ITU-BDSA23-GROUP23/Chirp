51ae460359cd0da1104afff3747b3dea15bc1039
ngar <ngar@itu.dk>
2023-10-10 14:18:46 +0200

    Test if Default page is the same as ?page=1
    Co-Authored-By: Daniel Holm Larsen <103388012+DHLarsen@users.noreply.github.com>
    Co-Authored-By: Edward Rostomian <36159443+EdwardR7@users.noreply.github.com>

--- Test/Chirp.Razor.Tests/APITests.cs
+++ Test/Chirp.Razor.Tests/APITests.cs
@@ -74,6 +74,39 @@ public class APITests
 
         Assert.NotEqual(HttpResponseBody2, HttpResponseBody);
 
+    }
+
+    [Fact]
+    public void defaultPageSameAsPage1() 
+    {
+
+          // Arrange
+        var url = "http://localhost:5273/?page=1"; //public timeline
+        var request = WebRequest.Create(url);
+        request.Method = "GET";
+
+        var url2 = "http://localhost:5273/"; //public timeline
+        var request2 = WebRequest.Create(url2);
+        request2.Method = "GET";
+
+
+        // Act
+        using var webResponse = request.GetResponse(); //returns a web response containing the response to the request.
+        using var webStream = webResponse.GetResponseStream(); //gets the instance of the stream class for reading data
+
+        using var reader = new StreamReader(webStream);
+        var HttpResponseBody = reader.ReadToEnd(); //We read all the data from the stream.
+
+        using var webResponse2 = request2.GetResponse(); //returns a web response containing the response to the request.
+        using var webStream2 = webResponse2.GetResponseStream(); //gets the instance of the stream class for reading data
+
+        using var reader2 = new StreamReader(webStream2);
+        var HttpResponseBody2 = reader2.ReadToEnd(); //We read all the data from the stream.
+
+        Assert.Equal(HttpResponseBody2, HttpResponseBody);
+
+
+
 
     }
 
