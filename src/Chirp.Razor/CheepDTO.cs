22704c09825d5472c4d11ee7bb15e8b5850f699f
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-10-10 11:54:48 +0200

    Update CheepDTO.cs

--- src/Chirp.Razor/CheepDTO.cs
+++ src/Chirp.Razor/CheepDTO.cs
@@ -5,15 +5,26 @@ public class CheepDTO
     public int Id { get; set; }
     public string Message { get; set; }
     public long TimeStamp { get; set; }
-    public string AuthorName { get; set; }
-    public string AuthorEmail { get; set; }
+    public string? AuthorName { get; set; }
+    public string? AuthorEmail { get; set; }
 
 
-    public CheepDTO(Cheep cheap)
+    public CheepDTO(Cheep cheep)
     {
-        Message = cheap.Message;
-        TimeStamp = ((DateTimeOffset)cheap.TimeStamp).ToUnixTimeMilliseconds();
-        AuthorName = cheap.Author.Name;
-        AuthorEmail = cheap.Author.Email;
+        Message = cheep.Message;
+        TimeStamp = ((DateTimeOffset)cheep.TimeStamp).ToUnixTimeMilliseconds();
+        AuthorName = cheep.Author.Name;
+        AuthorEmail = cheep.Author.Email;
     }
+
+    public string GetMessage()
+    {
+        return Message;
+    }
+
+    public long GetTimeStamp()
+    {
+        return TimeStamp;
+    }
+
 }
