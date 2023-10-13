ba0d96ff2a21fe553a24dab462f5290bd8623448
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-10-13 14:59:44 +0200

    Test files for repository method testing

---
+++ Test/Chirp.Razor.Tests/AuthorRepositoryTests.cs
@@ -0,0 +1,26 @@
+namespace Chirp.Razor.Tests;
+using Xunit;
+using System.Net;
+public class AuthorRepositoryTests
+{
+
+    [Fact]
+    public void CreateAuthorTest()
+    {
+
+    }
+
+    [Fact]
+    public void FindAuthorByEmail()
+    {
+
+    }
+
+    [Fact]
+    public void FindAuthorByName()
+    {
+
+    }
+
+
+}
---
+++ Test/Chirp.Razor.Tests/CheepRepositoryTests.cs
@@ -0,0 +1,32 @@
+namespace Chirp.Razor.Tests;
+using Xunit;
+using System.Net;
+public class CheepRepositoryTests
+{
+
+    [Fact]
+    public void CreateCheepTest()
+    {
+
+    }
+
+    [Fact]
+    public void GetCheepsTest()
+    {
+
+    }
+
+    [Fact]
+    public void CalculateSkippedCheepsTest()
+    {
+
+    }
+
+    [Fact]
+    public void CheepsToCheepDTOsTest()
+    {
+
+    }
+
+
+}
