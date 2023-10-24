2d6eebe27fa4448ccb2c713da9ca84ea2ec11959
habr <habr@itu.dk>
2023-10-24 11:43:23 +0200

    Added infrastructure for CheepRepository Unit tests
    Also added EntityFrameworkCore.InMemory to project
    
    Co-authored-by: ngar <ngar@itu.dk>

--- Test/Chirp.Razor.Tests/CheepRepositoryTests.cs
+++ Test/Chirp.Razor.Tests/CheepRepositoryTests.cs
@@ -1,19 +1,27 @@
 namespace Chirp.Razor.Tests;
 using Xunit;
 using System.Net;
+using Chirp.Web.data;
+using Chirp.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 public class CheepRepositoryTests
 {
 
     [Fact]
     public void CreateCheepTest()
     {
-
+        
     }
 
     [Fact]
     public void GetCheepsTest()
     {
-
+        var optionsBuilder = new DbContextOptionsBuilder<ChirpDBContext>().UseInMemoryDatabase("Data source = /tmp/chirp.db");
+        using (var db = new ChirpDBContext(optionsBuilder.Options))
+        {
+              
+        }
     }
 
     [Fact]
