8b123cf400edd9283eb114f9dba56da23a7070c0
tpep <tpep@itu.dk>
2023-10-27 11:45:34 +0200

    Cheep Required and Email MaxMinLen DataAnnotations

--- src/Chirp.Infrastructure/Cheep.cs
+++ src/Chirp.Infrastructure/Cheep.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Infrastructure
 {
     public class Cheep
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [Required]
         public required Author Author { get; set; }
-        public required string Message { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        [MinLength(1)]
+        public string Message { get; set; }
+
+        [Required]
         public DateTime TimeStamp { get; set; }
     }
 }
