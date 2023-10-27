be92cfb77bfeb263ee120471b7d8b004ac128e68
tpep <tpep@itu.dk>
2023-10-27 11:43:54 +0200

    Author Required and Email DataAnnotations

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Infrastructure
 {
     public class Author
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [Required]
         public required string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public required string Email { get; set; }
-        public required IEnumerable<Cheep> Cheeps { get; set; }
+
+        public IEnumerable<Cheep>? Cheeps { get; set; }
     }
 }
