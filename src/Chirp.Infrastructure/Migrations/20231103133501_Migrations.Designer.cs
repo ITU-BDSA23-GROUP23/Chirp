91b6114b4cd9f24e823e5a00c2331bc1fee9f2ff
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-11-03 14:40:02 +0100

    dotnet run på Chirp.WEB laver stadig error :/

--- src/Chirp.Infrastructure/Migrations/20231103133501_Migrations.Designer.cs
+++ src/Chirp.Infrastructure/Migrations/20231103133501_Migrations.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Infrastructure.Migrations
 {
     [DbContext(typeof(ChirpDBContext))]
-    [Migration("20231103131908_Migrations")]
+    [Migration("20231103133501_Migrations")]
     partial class Migrations
     {
         /// <inheritdoc />
