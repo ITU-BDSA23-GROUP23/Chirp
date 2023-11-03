6984d14288a0cd3e2325cb73fc86ba54e2d27819
tpep <tpep@itu.dk>
2023-11-03 15:17:44 +0100

    Added TotalPages to the html Models

--- src/Chirp.Web/CheepService.cs
+++ src/Chirp.Web/CheepService.cs
@@ -1,4 +1,5 @@
 using Chirp.Core;
+using Chirp.Infrastructure;
 
 
 public interface ICheepService : IDisposable
@@ -6,20 +7,26 @@ public interface ICheepService : IDisposable
     public Task<IEnumerable<CheepDTO>> GetCheeps(int page);
     // public List<DBFacade.CheepViewModel> GetCheepsFromAuthor(string author);
     public Task<IEnumerable<CheepDTO>> GetCheepsFromAuthor(string authorName, int page);
+
+    public Task<int> GetPageAmount(String? authorName = null);
 }
 
 public class CheepService : ICheepService
 {
+    int PageSize = 32; // This is only used in CheepService, not by the Repositeries that it uses, at this point. In the future it should be used in the repositories.
     private readonly ICheepRepository cheepRepository;
-    public CheepService(ICheepRepository cheepRepository)
+    private readonly IAuthorRepository authorRepository;
+    public CheepService(ICheepRepository cheepRepository, IAuthorRepository authorRepository)
     {
         this.cheepRepository = cheepRepository;
+        this.authorRepository = authorRepository;
+
         //context = new();
     }
     public async Task<IEnumerable<CheepDTO>> GetCheeps(int page)
     {
         return await cheepRepository.GetCheeps(page);
-        
+
     }
 
     public async Task<IEnumerable<CheepDTO>> GetCheepsFromAuthor(string authorName, int page)
@@ -34,7 +41,21 @@ public class CheepService : ICheepService
         dateTime = dateTime.AddSeconds(unixTimeStamp);
         return dateTime.ToString("MM/dd/yy H:mm:ss");
     }
-    
+
+    public async Task<int> GetPageAmount(String? authorName = null)
+    {
+        long cheepAmount;
+        if (authorName == null)
+        {
+            cheepAmount = await cheepRepository.GetCheepsAmount();
+        }
+        else
+        {
+            cheepAmount = await authorRepository.GetCheepAmount(authorName);
+        }
+        return (int)Math.Ceiling((double)cheepAmount / PageSize);
+    }
+
     public void Dispose()
     {
         throw new NotImplementedException();
