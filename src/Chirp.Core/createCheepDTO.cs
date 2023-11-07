b301db8b9145c3fecbc2896c05b7c2f204a629db
tpep <tpep@itu.dk>
2023-11-07 12:40:57 +0100

    FluentValidation CheepDTO

--- src/Chirp.Core/createCheepDTO.cs
+++ src/Chirp.Core/createCheepDTO.cs
@@ -1,5 +1,23 @@
 namespace Chirp.Core
 {
-    public record createCheepDTO(AuthorDTO AuthorDTO, string Message);
+    using FluentValidation;
+    public class createCheepDTO
+    {
+        public AuthorDTO Author { get; set; }
+        public string Message { get; set; }
+        public createCheepDTO(AuthorDTO AuthorDTO, string Message)
+        {
+            this.Author = AuthorDTO;
+            this.Message = Message;
+        }
+    }
 
+    public class createCheepDTOValidator : AbstractValidator<createCheepDTO> // To use validator: https://docs.fluentvalidation.net/en/latest/start.html
+    {
+        public createCheepDTOValidator()
+        {
+            RuleFor(createCheepDTO => createCheepDTO.Message).MinimumLength(1);
+            RuleFor(createCheepDTO => createCheepDTO.Message).MaximumLength(160);
+        }
+    }
 }
