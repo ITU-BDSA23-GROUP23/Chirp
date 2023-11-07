ee1915d7fa11c1f46f68d3761733b3fd6aba5cf9
Nicklas~ <gardilnicklas6@gmail.com>
2023-11-07 15:19:50 +0100

    Now a user can send a cheep
    There no validation currently

--- src/Chirp.Web/Pages/CreateCheep.cshtml.cs
+++ src/Chirp.Web/Pages/CreateCheep.cshtml.cs
@@ -15,20 +15,41 @@ public class CreateCheepModel : PageModel {
 
 
     private readonly ICheepService _service;
+
+    private readonly IAuthorRepository _Author_repository;
+
+    private readonly ICheepRepository _Cheep_repository;
     private readonly ILogger<PublicModel> _logger;
 
-    public CreateCheepModel(ICheepService service, ILogger<PublicModel> logger)
+    public CreateCheepModel(ICheepService service, ILogger<PublicModel> logger, IAuthorRepository Author_repository, ICheepRepository Cheep_repository)
     {
         _service = service;
         _logger = logger;
+        _Author_repository = Author_repository;
+        _Cheep_repository =  Cheep_repository;
 
     }
 
 
+    public async void createCheep(string UserName, string message) {
+        var _Author = _Author_repository.FindAuthorByName(UserName);
+        _Author.Wait();
+        var Author = _Author.Result;
+        Console.WriteLine("Author:  " +Author);
+        Console.WriteLine("Message:  " +message);  
+        if(Author!= null) {
+            _Cheep_repository.CreateCheep(Author, message);
+        } else {
+            string email = UserName + "email.com";
+            Author = new AuthorDTO(UserName, email);
+            _Author_repository.CreateAuthor(Author);
+            _Cheep_repository.CreateCheep(Author, message);
+        }
+    }
+     public ActionResult OnGet()
+    {
+        
+        return Page();
+    
 
-
-
-
-
-
-}
+} }
