64670fa4cdc2f84f598431a0f8a815e9ac0cc6eb
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-11-07 14:17:33 +0100

    CreateCheepTest done

---
+++ Test/Chirp.Razor.Tests/CreateCheepTest.cs
@@ -0,0 +1,47 @@
+namespace Chirp.Razor.Tests;
+using Xunit;
+using Microsoft.Data.Sqlite;
+using Chirp.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using Chirp.Core;
+using System.Data.Common;
+using Chirp.Web.data;
+public class CreateCheepTest
+{
+    ChirpDBContext context;
+    SqliteConnection _connection;
+    public CreateCheepTest()
+    {
+        _connection = new SqliteConnection("Filename=:memory:");
+        _connection.Open();
+        var _contextOptions = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(_connection).Options;
+        context = new ChirpDBContext(_contextOptions);
+        context.Database.EnsureCreated();
+    }
+
+    [Fact]
+    public async void CreateCheep()
+    {
+        //Arrange
+        string Message = "MyMessage";
+        ICheepRepository cheepRepository = new CheepRepository(context);
+        IAuthorRepository authorRepository = new AuthorRepository(context);
+        AuthorDTO author = new AuthorDTO("Thorstein Pedersen", "tpepsi@gmail.com");
+
+        authorRepository.CreateAuthor(author);
+
+        //Act
+        cheepRepository.CreateCheep(author, Message);
+
+        //Assert
+        var cheeps = await cheepRepository.GetCheeps();
+        foreach (var cheepDTO in cheeps)
+        {
+            Assert.True(cheepDTO.AuthorName == "Thorstein Pedersen" && cheepDTO.Message == "MyMessage");
+        }
+
+    }
+
+
+}
