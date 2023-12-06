f13feca6e2aee48f1a88ae42c286f064a487a4b0
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-12-06 11:29:25 +0100

    setup before break

---
+++ Test/Chirp.Razor.Tests/PageInfoTests.cs
@@ -0,0 +1,58 @@
+namespace Chirp.Razor.Tests;
+using Xunit;
+using Chirp.Infrastructure;
+
+public class PageInfoTests
+{
+    private readonly ChirpDBContext dbContext;
+
+    public PageInfoTests(ChirpDBContext dbContext)
+    {
+        this.dbContext = dbContext;
+
+    }
+    [Fact]
+    public void OnGetAuthorTest()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+    }
+
+    [Fact]
+    public void OnGetTest()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+    }
+
+    [Fact]
+    public void FollowingCountTest()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void FollowersCountTest()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
