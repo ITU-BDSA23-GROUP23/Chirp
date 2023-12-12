ff2e48223896f9267ef9689694e1db708dfe7d90
ngar <ngar@itu.dk>
2023-12-12 15:43:55 +0100

    Removed warnings
    Co-Authored-By: Halfdan Brage <123736044+HalfdanBrage@users.noreply.github.com>

--- src/Chirp.Web/Pages/ForYou.cshtml.cs
+++ src/Chirp.Web/Pages/ForYou.cshtml.cs
@@ -13,8 +13,8 @@ public class ForYouModel : PageModel
     private readonly IAuthorRepository authorRepository;
     private readonly ICheepRepository cheepRepository;
     public PageNavModel PageNav;
-    public IEnumerable<CheepDTO> Cheeps;
-    public string author;
+    public IEnumerable<CheepDTO>? Cheeps;
+    public string? author;
 
     public ForYouModel(IAuthorRepository authorRepository, ICheepRepository cheepRepository) 
         {
@@ -29,11 +29,11 @@ public class ForYouModel : PageModel
         var TotalPages = await cheepRepository.GetPageAmount();
         try
         {
-            var followingAuthors = (await authorRepository.FindAuthorByName(authorName)).Following;
+            var followingAuthors = (await authorRepository.FindAuthorByName(authorName))!.Following;
 
             long totalCheeps = 0;
 
-            foreach (var author in followingAuthors)
+            foreach (var author in followingAuthors!)
             {
                totalCheeps += await authorRepository.GetCheepAmount(await authorRepository.GetAuthorName(author)); 
             }
@@ -61,25 +61,25 @@ public class ForYouModel : PageModel
         lo = HttpContext.Request.Query["lo"].ToString();
         if (string.IsNullOrEmpty(li))
         {
-            li = Request.Form["Like"];
+            li = Request.Form["Like"]!;
         }
 
         if (string.IsNullOrEmpty(di))
         {
-            di = Request.Form["Dislike"];
+            di = Request.Form["Dislike"]!;
         }
 
         if (string.IsNullOrEmpty(lo))
         {
-            lo = Request.Form["Love"];
+            lo = Request.Form["Love"]!;
         }
         
         if (f != null)
         {
-            await Methods.FollowAuthor(authorRepository, f, Request.Form["Follow"]);
+            await Methods.FollowAuthor(authorRepository, f, Request.Form["Follow"]!);
         } else if (uf != null)
         {
-            await Methods.UnfollowAuthor(authorRepository, uf, Request.Form["Unfollow"]);
+            await Methods.UnfollowAuthor(authorRepository, uf, Request.Form["Unfollow"]!);
         } 
         else if(li != null)
         {
